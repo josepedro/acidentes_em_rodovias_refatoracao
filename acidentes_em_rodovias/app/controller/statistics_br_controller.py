e2a8add9cc5b88659cb17c035526599157b97a6e
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-24 19:54:49 -0300

    Applying coding patterns in estatisticas_br_controller.py

---
+++ acidentes_em_rodovias/app/controller/statistics_br_controller.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+
+from django.utils.datastructures import MultiValueDictKeyError
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from exception.validation_exceptions import *
+from exception.internal_exceptions import *
+
+from models.dao.br_acidentes_dao import *
+
+from datetime import datetime
+
+import MySQLdb
+import logging
+import sys
+import os
+import inspect
+
+# Add upper directories
+current_dir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())
+))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+# Config logging messages
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+
+def accidents_br(request):
+    try:
+        date = datetime.now()
+        br_dao = BRAccidentsDAO()
+        general_accidents = br_dao.general_accidents()()
+        accidents_year = br_dao.general_accidents_year()()
+
+    except (MySQLdb.Error, ResultadoConsultaNuloError), e:
+        logger.error(str(e))
+        error = "Ocorreu um erro no sistema, tente novamente mais tarde!"
+        return render_to_response("index.html", {
+            'error': error
+        }, context_instance=RequestContext(request))
+
+    return render_to_response("br_acidentes.html", {
+        'year': range(2007, date.year + 1),
+        'general_accidents': general_accidents,
+        'accidents_year': accidents_year
+    }, context_instance=RequestContext(request))
