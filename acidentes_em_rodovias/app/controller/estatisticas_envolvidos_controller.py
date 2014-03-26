e8e610bd58aabfca503658eb09b152f42902a97c
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 18:47:52 -0300

    Applying code patterns in estatisticas_envolvidos_controller.py

--- acidentes_em_rodovias/app/controller/estatisticas_envolvidos_controller.py
+++ acidentes_em_rodovias/app/controller/estatisticas_envolvidos_controller.py
@@ -1,46 +1,85 @@
-# -*- coding: utf-8 -*- 
-import sys, os, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
 
+import sys
+import os
+import inspect
 import MySQLdb
+import logging
+
 from django.utils.datastructures import MultiValueDictKeyError
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+
 from exception.validation_exceptions import *
 from exception.internal_exceptions import *
+
 from models.dao.envolvidos_acidentes_dao import *
 from models.dao.pessoas_acidentes_dao import *
-from datetime import datetime
-import logging
 
+from datetime import datetime
+
+# Adding upper directories to the Python Path
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
+
+# Logging config
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
+
 def ocorrencias_e_envolvidos(request):
-	envolvidosDAO = EnvolvidosAcidentesDAO()
-	lista_envolvidos = envolvidosDAO.envolvidos_acidentes()
-	medias, desvio = envolvidosDAO.media_desvio_envolvidos()
-	anos = [i.ano for i in lista_envolvidos]
+    envolvidosDAO = EnvolvidosAcidentesDAO()
+    lista_envolvidos = envolvidosDAO.envolvidos_acidentes()
+    medias, desvio = envolvidosDAO.media_desvio_envolvidos()
+    anos = []
 
-	ano_media_list = zip(anos, medias)
+    for acidente in lista_envolvidos:
+        anos.append(acidente.ano)
 
-	return render_to_response("ocorrencias-e-envolvidos.html",{'lista_envolvidos':lista_envolvidos, 'ano_media_list':ano_media_list, 'desvio':desvio}, context_instance=RequestContext(request))	
+    ano_media_list = zip(anos, medias)
+
+    return render_to_response(
+        "ocorrencias-e-envolvidos.html", {
+            'lista_envolvidos': lista_envolvidos,
+            'ano_media_list': ano_media_list,
+            'desvio': desvio
+        }, context_instance=RequestContext(request)
+    )
+
 
 def acidentes_sexo(request):
-	try:
-		pessoas_dao = PessoasAcidentesDAO()
-		homens_ano = pessoas_dao.acidentes_por_sexo_e_ano('M')
-		mulheres_ano = pessoas_dao.acidentes_por_sexo_e_ano('F')
-		homens_geral = pessoas_dao.acidentes_por_sexo_geral('M')
-		mulheres_geral = pessoas_dao.acidentes_por_sexo_geral('F')
-		homens_geral, mulheres_geral = media_sexo(homens_geral, mulheres_geral)
+    try:
+        pessoas_dao = PessoasAcidentesDAO()
+        homens_ano = pessoas_dao.acidentes_por_sexo_e_ano('M')
+        mulheres_ano = pessoas_dao.acidentes_por_sexo_e_ano('F')
+        homens_geral = pessoas_dao.acidentes_por_sexo_geral('M')
+        mulheres_geral = pessoas_dao.acidentes_por_sexo_geral('F')
+        homens_geral, mulheres_geral = media_sexo(homens_geral, mulheres_geral)
 
-	except (MySQLdb.Error, ResultadoConsultaNuloError), e:
-		logger.error(str(e))
-		erro = "Ocorreu um erro no sistema, tente novamente mais tarde!"
-		return render_to_response("index.html", {'erro' : erro}, context_instance=RequestContext(request))
+    except (MySQLdb.Error, ResultadoConsultaNuloError), e:
+        logger.error(str(e))
+        erro = "Ocorreu um erro no sistema, tente novamente mais tarde!"
+        return render_to_response(
+            "index.html", {
+                'erro': erro
+            }, context_instance=RequestContext(request)
+        )
 
-	return render_to_response("acidentes_sexo.html",{'homens_geral':homens_geral, 'mulheres_geral':mulheres_geral, 'homens_ano':homens_ano, 'mulheres_ano':mulheres_ano}, context_instance=RequestContext(request))
+    return render_to_response(
+        "acidentes_sexo.html", {
+            'homens_geral': homens_geral,
+            'mulheres_geral': mulheres_geral,
+            'homens_ano': homens_ano,
+            'mulheres_ano': mulheres_ano
+        }, context_instance=RequestContext(request)
+    )
