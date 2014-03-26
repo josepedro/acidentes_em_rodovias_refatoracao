4c3dcd6a2bf393163545592dcda0f527cbf9b015
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-25 23:51:36 -0300

    Applying code patterns in pessoas_acidentes_dao.py

--- acidentes_em_rodovias/app/models/dao/pessoas_acidentes_dao.py
+++ acidentes_em_rodovias/app/models/dao/pessoas_acidentes_dao.py
@@ -1,20 +1,39 @@
 # -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
 
 from generico_dao import *
 
+
 class PessoasAcidentesDAO(GenericoDAO):
-	def acidentes_por_sexo_e_ano(self, sexo):
-		query = """SELECT a.ano, a.sexo, a.quantidade 
-				FROM acidentes_por_sexo AS a
-				WHERE a.sexo = '%s'
-				ORDER BY a.ano;""" % (sexo)
 
-		return self.transforma_dicionario_em_objetos(self.executa_query(query), "PessoasAcidentes", "pessoas_acidentes")
+    """Pessoas em Acidentes DAO"""
 
-	def acidentes_por_sexo_geral(self, sexo):
-		query = """SELECT a.sexo, SUM(a.quantidade) AS quantidade
-				FROM acidentes_por_sexo AS a
-				WHERE a.sexo = '%s'
-				ORDER BY a.ano;""" % (sexo)
+    def acidentes_por_sexo_e_ano(self, sexo):
+        query = """SELECT a.ano, a.sexo, a.quantidade
+                FROM acidentes_por_sexo AS a
+                WHERE a.sexo = '%s'
+                ORDER BY a.ano;""" % (sexo)
 
-		return self.transforma_dicionario_em_objetos(self.executa_query(query), "PessoasAcidentesGeral", "pessoas_acidentes")
+        return self.transforma_dicionario_em_objetos(
+            self.executa_query(query),
+            "PessoasAcidentes",
+            "pessoas_acidentes"
+        )
+
+    def acidentes_por_sexo_geral(self, sexo):
+        query = """SELECT a.sexo, SUM(a.quantidade) AS quantidade
+                FROM acidentes_por_sexo AS a
+                WHERE a.sexo = '%s'
+                ORDER BY a.ano;""" % (sexo)
+
+        return self.transforma_dicionario_em_objetos(
+            self.executa_query(query),
+            "PessoasAcidentesGeral",
+            "pessoas_acidentes"
+        )
