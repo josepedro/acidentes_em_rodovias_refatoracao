9d275ba8c417e65972228d3bb616250c562addf4
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-25 22:37:23 -0300

    Applying code patterns in br_acidentes.py

--- acidentes_em_rodovias/app/models/br_acidentes.py
+++ acidentes_em_rodovias/app/models/br_acidentes.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
-"""@package Modelo de acidentes
-Declaração das classes para acidentes.
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
 
-Este modulo contem declação das classes de modelo
-para acidentes em BRs
-"""
-class BRAcidentes(object):
-	""" Acidentes em geral das rodovias"""
-	def __init__(self):
-			self.quantidade_ocorrencias = ''
-			self.br = ''
 
-class BRAcidentesAno(object):
-	""" Acidentes em geral das rodovias separados por ano"""
-	def __init__(self):
-			self.quantidade_ocorrencias_list = []
-			self.br= ''
-			self.ano_list = []
+class BRAcidentes():
+
+    """ Acidentes em geral das rodovias"""
+
+    def __init__(self):
+        self.quantidade_ocorrencias = ''
+        self.br = ''
+
+
+class BRAcidentesAno():
+
+    """ Acidentes em geral das rodovias separados por ano"""
+
+    def __init__(self):
+        self.quantidade_ocorrencias_list = []
+        self.br = ''
+        self.ano_list = []
