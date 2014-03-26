696d95f96558577d6624285fca1bc995350f5a45
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-25 23:17:36 -0300

    Applying code patterns in causas_acidentes_dao.py

--- acidentes_em_rodovias/app/models/dao/causas_acidentes_dao.py
+++ acidentes_em_rodovias/app/models/dao/causas_acidentes_dao.py
@@ -1,117 +1,174 @@
 # -*- coding: utf-8 -*-
-"""@package Causas Acidentes DAO
-Data Access Object (DAO) para causa de acidentes nas BRs.
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
 
-Este modulo contem declação da classe que acessa os
-dados no banco e  os exporta para a controller
-"""
+import sys
+import os
+import inspect
 
-from generico_dao import GenericoDAO
-import sys, os, inspect
+# Adding upper directories to the Python Path
 current_path = os.path.dirname(os.path.abspath('..'))
 sys.path.append(current_path)
 current_path = os.path.dirname(os.path.abspath('.'))
 sys.path.append(current_path)
 
+from generico_dao import GenericoDAO
+
 from models.causas_acidentes import *
+
 from util.estatisticas_util import *
 
+
 class CausasAcidentesDAO(GenericoDAO):
-	def causas_acidentes(self):
-		""" Causa de Acidentes 
 
-			Efetua uma query buscando por causa de acidentes em geral
-			@return lista de objetos para as respostas da query
-		"""
-		query = """SELECT causa, SUM(quantidade_ocorrencias) AS quantidade_ocorrencias
-				 FROM estatisticas_causa
-				 GROUP BY  causa
-				 ORDER BY quantidade_ocorrencias DESC; """
+    def causas_acidentes(self):
+        query = """SELECT causa,
+                    SUM(quantidade_ocorrencias) AS quantidade_ocorrencias
+                FROM estatisticas_causa
+                GROUP BY  causa
+                ORDER BY quantidade_ocorrencias DESC; """
 
-		return self.transforma_dicionario_em_objetos(self.executa_query(query), "Acidentes", "causas_acidentes")
+        return self.transforma_dicionario_em_objetos(
+            self.executa_query(query),
+            "Acidentes",
+            "causas_acidentes"
+        )
 
-	def causas_acidentes_ano(self):
-		""" Causa de Acidentes por Ano
+    def causas_acidentes_ano(self):
+        query = """SELECT causa, quantidade_ocorrencias, ano
+                FROM estatisticas_causa
+                ORDER BY causa, ano ; """
 
-			Efetua uma query buscando por causa de acidentes em geral
-			organizadas por ano
-			@return lista de objetos para as respostas da query
-		"""
-		query = """SELECT causa, quantidade_ocorrencias, ano
-				 FROM estatisticas_causa 
-				 ORDER BY causa, ano ; """
-		
-		resultado_query = self.executa_query(query)
+        resultado_query = self.executa_query(query)
 
-		causas_acidentes_ano_list = []
-		ultimo_causa = ''
-		for (causa, quantidade_ocorrencias, ano) in zip(resultado_query['causa'].values(), resultado_query['quantidade_ocorrencias'].values(), resultado_query['ano'].values()):
-			causa = causa.decode('iso-8859-1').encode('utf8')
-			if (ultimo_causa != causa):
-				causas_acidentes_ano =  AcidentesAno()
-				causas_acidentes_ano_list.append(causas_acidentes_ano)
-				causas_acidentes_ano.causa = causa
-				ultimo_causa = causa
-			causas_acidentes_ano.ano_list.append(ano)
-			causas_acidentes_ano.quantidade_ocorrencias_list.append(quantidade_ocorrencias)
+        causas_acidentes_ano_list = []
+        ultimo_causa = ''
 
-		return causas_acidentes_ano_list
+        for (causa, quantidade_ocorrencias, ano) in zip(
+            resultado_query['causa'].values(),
+            resultado_query['quantidade_ocorrencias'].values(),
+            resultado_query['ano'].values()
+        ):
+            causa = causa.decode('iso-8859-1').encode('utf8')
+            if (ultimo_causa != causa):
+                causas_acidentes_ano = AcidentesAno()
+                causas_acidentes_ano_list.append(causas_acidentes_ano)
+                causas_acidentes_ano.causa = causa
+                ultimo_causa = causa
 
-	def probabilidade_causas_acidentes(self):
-		""" Probabilidade Causa de Acidentes 
+            causas_acidentes_ano.ano_list.append(ano)
+            causas_acidentes_ano.quantidade_ocorrencias_list.append(
+                quantidade_ocorrencias
+            )
 
-			Efetua uma query buscando por causa de acidentes em geral.
-			Trabalha com o retorno em Data Frame para levantar as
-			estatisticas de causas de acidentes.
-			@return lista de objetos para as respostas da query
-		"""
-		query = """SELECT causa, quantidade_ocorrencias, ano
-				 FROM estatisticas_causa 
-				 ORDER BY causa, ano ; """
-		
-		data_frame = self.executa_query(query, get_data_frame=True)
-		medias_list = data_frame.groupby('causa')['quantidade_ocorrencias'].mean()
-		desvios_padroes_list = data_frame.groupby('causa')['quantidade_ocorrencias'].std()
-		probabilidade_causas_acidentes_list = []
-		
-		for i in range(0, len(medias_list)):
-			probabilidade_causas_acidentes = ProbabilidadeAcidentes()
-			probabilidade_causas_acidentes.causa = medias_list.keys()[i].decode('iso-8859-1').encode('utf8')
-			
-			limites = [(0,5000), (5001,10000), (10001,30000), (30001,50000), (50001, sys.maxint)]
-			for (inferior, superior) in limites:
-				if (medias_list[i] >= inferior and medias_list[i] <= superior):
-					probabilidade_causas_acidentes.probabilidade_por_limite_list.append(100 * (distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i]) + distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i])))
-				elif (medias_list[i] >= inferior):
-					probabilidade_causas_acidentes.probabilidade_por_limite_list.append(100 * (distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i]) - distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i])))
-				elif (medias_list[i] <= inferior):
-					probabilidade_causas_acidentes.probabilidade_por_limite_list.append(100 * (distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i]) - distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i])))	
-			
-			probabilidade_causas_acidentes_list.append(probabilidade_causas_acidentes)
+        return causas_acidentes_ano_list
 
-		return probabilidade_causas_acidentes_list
-	
-	def media_desvio_causas_acidentes(self):
-		""" Média e Desvio Padrão de Causa de Acidentes 
+    def probabilidade_causas_acidentes(self):
+        query = """SELECT causa, quantidade_ocorrencias, ano
+                FROM estatisticas_causa
+                ORDER BY causa, ano ; """
 
-			Efetua uma query buscando por causa de acidentes em geral.
-			Trabalha com o retorno em Data Frame para levantar as
-			estatisticas de causas de acidentes.
-			@return lista de objetos para as respostas da query
-		"""
-		query = """SELECT causa, quantidade_ocorrencias, ano
-				 FROM estatisticas_causa 
-				 ORDER BY causa, ano ; """		
-		
-		data_frame = self.executa_query(query, get_data_frame=True)
-		medias_list = data_frame.groupby('causa')['quantidade_ocorrencias'].mean()
-		desvios_padroes_list = data_frame.groupby('causa')['quantidade_ocorrencias'].std()
-		media_desvio_causas_acidentes_list = []
-		for i in range(0, len(medias_list)):
-			media_desvio_causas_acidentes = MediaDesvioAcidentes()
-			media_desvio_causas_acidentes.media = medias_list[i]
-			media_desvio_causas_acidentes.desvio = desvios_padroes_list[i]
+        data_frame = self.executa_query(query, get_data_frame=True)
+        medias_list = data_frame.groupby(
+            'causa'
+        )['quantidade_ocorrencias'].mean()
 
-			media_desvio_causas_acidentes_list.append(media_desvio_causas_acidentes)
+        desvios_padroes_list = data_frame.groupby(
+            'causa'
+        )['quantidade_ocorrencias'].std()
 
-		return media_desvio_causas_acidentes_list
+        probabilidade_causas_acidentes_list = []
+
+        for i in range(0, len(medias_list)):
+            prob_causa_acidentes = ProbabilidadeAcidentes()
+            prob_causa_acidentes.causa = medias_list.keys()[
+                i].decode('iso-8859-1').encode('utf8')
+
+            limites = [
+                (0, 5000),
+                (5001, 10000),
+                (10001, 30000),
+                (30001, 50000),
+                (50001, sys.maxint)
+            ]
+            for (inferior, superior) in limites:
+                if (medias_list[i] >= inferior and medias_list[i] <= superior):
+                    prob_causa_acidentes.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) + distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+                elif (medias_list[i] >= inferior):
+                    prob_causa_acidentes.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) - distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+                elif (medias_list[i] <= inferior):
+                    prob_causa_acidentes.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) - distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+
+            probabilidade_causas_acidentes_list.append(
+                prob_causa_acidentes
+            )
+
+        return probabilidade_causas_acidentes_list
+
+    def media_desvio_causas_acidentes(self):
+        query = """SELECT causa, quantidade_ocorrencias, ano
+                FROM estatisticas_causa
+                ORDER BY causa, ano ; """
+
+        data_frame = self.executa_query(query, get_data_frame=True)
+
+        medias_list = data_frame.groupby(
+            'causa'
+        )['quantidade_ocorrencias'].mean()
+        desvios_padroes_list = data_frame.groupby(
+            'causa'
+        )['quantidade_ocorrencias'].std()
+
+        media_desvio_causas_acidentes_list = []
+
+        for i in range(0, len(medias_list)):
+            media_desvio_causas_acidentes = MediaDesvioAcidentes()
+            media_desvio_causas_acidentes.media = medias_list[i]
+            media_desvio_causas_acidentes.desvio = desvios_padroes_list[i]
+
+            media_desvio_causas_acidentes_list.append(
+                media_desvio_causas_acidentes
+            )
+
+        return media_desvio_causas_acidentes_list
