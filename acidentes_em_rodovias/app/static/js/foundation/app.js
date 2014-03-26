a1c32621e946ec24f45515d76e482d347dff0dee
Jose Pedro <1jpsneto@gmail.com>
2014-03-26 18:46:39 -0300

    Applied autoformat in javascript files. Indentation and patterns of spaces applied.

--- acidentes_em_rodovias/app/static/js/foundation/app.js
+++ acidentes_em_rodovias/app/static/js/foundation/app.js
@@ -3,37 +3,39 @@
  * Técnicas de Programação, 1/2014
  * Acidentes em Rodovias, 2013-2014
  * GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
-*/
+ */
+;
+(function ($, window, undefined) {
+    'use strict';
 
-;(function ($, window, undefined) {
-  'use strict';
+    var $doc = $(document),
+        Modernizr = window.Modernizr;
 
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
+    $(document).ready(function () {
+        $.fn.foundationAlerts ? $doc.foundationAlerts() : null;
+        $.fn.foundationButtons ? $doc.foundationButtons() : null;
+        $.fn.foundationAccordion ? $doc.foundationAccordion() : null;
+        $.fn.foundationNavigation ? $doc.foundationNavigation() : null;
+        $.fn.foundationTopBar ? $doc.foundationTopBar() : null;
+        $.fn.foundationCustomForms ? $doc.foundationCustomForms() : null;
+        $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+        $.fn.foundationTabs ? $doc.foundationTabs({
+            callback: $.foundation.customForms.appendCustomMarkup
+        }) : null;
+        $.fn.foundationTooltips ? $doc.foundationTooltips() : null;
+        $.fn.foundationMagellan ? $doc.foundationMagellan() : null;
+        $.fn.foundationClearing ? $doc.foundationClearing() : null;
 
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
-
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
-    $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
+        $.fn.placeholder ? $('input, textarea').placeholder() : null;
     });
-  }
+
+    // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+    if (Modernizr.touch && !window.location.hash) {
+        $(window).load(function () {
+            setTimeout(function () {
+                window.scrollTo(0, 1);
+            }, 0);
+        });
+    }
 
 })(jQuery, this);
