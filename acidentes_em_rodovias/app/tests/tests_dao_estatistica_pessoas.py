8b1753541095f9156d5c25e35ec31e4633b4edc2
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 20:31:45 -0300

    Applying code patterns in tests_dao_estatistica_pessoa.py

--- acidentes_em_rodovias/app/tests/tests_dao_estatistica_pessoas.py
+++ acidentes_em_rodovias/app/tests/tests_dao_estatistica_pessoas.py
@@ -1,45 +1,67 @@
-# -*- coding: utf-8 -*- 
-import sys, os, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+import sys
+import os
+import inspect
 
 from django.test import SimpleTestCase
 from django.core.urlresolvers import reverse, resolve
+
 from models.dao import pessoas_acidentes_dao as dao
 from models import pessoas_acidentes
+
 from _mysql_exceptions import OperationalError, ProgrammingError
+
 from exception.internal_exceptions import *
 
+# Adding upper directories to the Python Path
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
+
+
 class Test_Estatistica_Pessoa(SimpleTestCase):
-	"""docstring for Test_Estatistica_Pessoa
-	"""
-	def setUp(self):    #configura ambiente para teste
-		self.estatistica = dao.PessoasAcidentesDAO()
-		#descobre qual metodo será chamado e formata a saída
-		func = str(self.id).split('=')[-1][:-2]
-		func = func.split('test_')[-1]
-		func = func.replace('_',' ')
-		out = '\rTeste de ' + func + ' '
-		out = out.ljust(65,'-')
-		sys.stderr.write(out)
-		self.shortDescription()
 
-	def tearDown(self):
-		# informa que o teste foi realizado
-		sys.stderr.write('Done\n')
+    """docstring for Test_Estatistica_Pessoa
+    """
 
-	def shortDescription(self):
-		return "Teste da classe EstatisticasPessoasDAO"
+    def setUp(self):
+        self.estatistica = dao.PessoasAcidentesDAO()
+        func = str(self.id).split('=')[-1][:-2]
+        func = func.split('test_')[-1]
+        func = func.replace('_', ' ')
+        out = '\rTeste de ' + func + ' '
+        out = out.ljust(65, '-')
+        sys.stderr.write(out)
+        self.shortDescription()
 
-	def test_acidentes_por_sexo(self):
-		self.assertIsNotNone(self.estatistica)
+    def tearDown(self):
+        sys.stderr.write('Done\n')
 
-	def test_instancia_estatistica_pessoa(self):
-		with self.assertRaises(NameError):
-			self.assertIsNotNone(estatistica_pessoas.EstatisticaPessoas())
-		with self.assertRaises(NameError):
-			self.assertIsNotNone(str(estatistica_pessoas.EstatisticaPessoas()))
-		with self.assertRaises(NameError):
-			self.assertIsNotNone(str(estatistica_pessoas.PessoasAcidentesGeral()))
+    def shortDescription(self):
+        return "Teste da classe EstatisticasPessoasDAO"
 
+    def test_acidentes_por_sexo(self):
+        self.assertIsNotNone(self.estatistica)
+
+    def test_instancia_estatistica_pessoa(self):
+        with self.assertRaises(NameError):
+            self.assertIsNotNone(
+                estatistica_pessoas.EstatisticaPessoas()
+            )
+        with self.assertRaises(NameError):
+            self.assertIsNotNone(str(
+                estatistica_pessoas.EstatisticaPessoas()
+            ))
+        with self.assertRaises(NameError):
+            self.assertIsNotNone(str(
+                estatistica_pessoas.PessoasAcidentesGeral()
+            ))
