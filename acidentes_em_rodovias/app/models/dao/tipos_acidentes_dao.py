c063f03167a032f78fc6f4727045046056b17379
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 00:01:29 -0300

    Applying code patterns in tipos_acidentes_dao.py

--- acidentes_em_rodovias/app/models/dao/tipos_acidentes_dao.py
+++ acidentes_em_rodovias/app/models/dao/tipos_acidentes_dao.py
@@ -1,85 +1,176 @@
 # -*- coding: utf-8 -*-
-from generico_dao import GenericoDAO
-import sys, os, inspect
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+import sys
+import os
+import inspect
+
+# Adding upper directories to the Python Path
 current_path = os.path.dirname(os.path.abspath('..'))
 sys.path.append(current_path)
 current_path = os.path.dirname(os.path.abspath('.'))
 sys.path.append(current_path)
 
+from generico_dao import GenericoDAO
+
 from models.causas_acidentes import *
+
 from util.estatisticas_util import *
 
+
 class TiposAcidentesDAO(GenericoDAO):
-	def tipos_acidentes(self):
-		query = """SELECT tipo, SUM(quantidade_ocorrencias) AS quantidade_ocorrencias
-				 FROM estatisticas_tipo
-				 GROUP BY  tipo
-				 ORDER BY quantidade_ocorrencias DESC; """
 
-		return self.transforma_dicionario_em_objetos(self.executa_query(query), "Acidentes", "causas_acidentes")
+    """Tipos de Acidentes DAO"""
 
-	def tipos_acidentes_ano(self):
-		query = """SELECT tipo, quantidade_ocorrencias, ano
-				 FROM estatisticas_tipo 
-				 ORDER BY tipo, ano ; """
-		
-		resultado_query = self.executa_query(query)
+    def tipos_acidentes(self):
+        query = """SELECT tipo,
+                    SUM(quantidade_ocorrencias) AS quantidade_ocorrencias
+                FROM estatisticas_tipo
+                GROUP BY  tipo
+                ORDER BY quantidade_ocorrencias DESC; """
 
-		tipos_acidentes_ano_list = []
-		ultimo_tipo = ''
-		for (tipo, quantidade_ocorrencias, ano) in zip(resultado_query['tipo'].values(), resultado_query['quantidade_ocorrencias'].values(), resultado_query['ano'].values()):
-			tipo = tipo.decode('iso-8859-1').encode('utf8')
-			if (ultimo_tipo != tipo):
-				tipos_acidentes_ano =  AcidentesAno()
-				tipos_acidentes_ano_list.append(tipos_acidentes_ano)
-				tipos_acidentes_ano.tipo = tipo
-				ultimo_tipo = tipo
-			tipos_acidentes_ano.ano_list.append(ano)
-			tipos_acidentes_ano.quantidade_ocorrencias_list.append(quantidade_ocorrencias)
+        return self.transforma_dicionario_em_objetos(
+            self.executa_query(query),
+            "Acidentes",
+            "causas_acidentes"
+        )
 
-		return tipos_acidentes_ano_list
+    def tipos_acidentes_ano(self):
+        query = """SELECT tipo, quantidade_ocorrencias, ano
+                FROM estatisticas_tipo
+                ORDER BY tipo, ano ; """
 
-	def probabilidade_tipos_acidentes(self):
-		query = """SELECT tipo, quantidade_ocorrencias, ano
-				 FROM estatisticas_tipo 
-				 ORDER BY tipo, ano ; """
-		
-		data_frame = self.executa_query(query, get_data_frame=True)
-		medias_list = data_frame.groupby('tipo')['quantidade_ocorrencias'].mean()
-		desvios_padroes_list = data_frame.groupby('tipo')['quantidade_ocorrencias'].std()
-		probabilidade_tipo_list = []
-		
-		for i in range(0, len(medias_list)):
-			probabilidade_tipo = ProbabilidadeAcidentes()
-			probabilidade_tipo.tipo = medias_list.keys()[i].decode('iso-8859-1').encode('utf8')
-			
-			limites = [(0,1000), (1001,5000), (5001,10000), (10001,50000), (50001, sys.maxint)]
-			for (inferior, superior) in limites:
-				if (medias_list[i] >= inferior and medias_list[i] <= superior):
-					probabilidade_tipo.probabilidade_por_limite_list.append(100 * (distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i]) + distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i])))
-				elif (medias_list[i] >= inferior):
-					probabilidade_tipo.probabilidade_por_limite_list.append(100 * (distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i]) - distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i])))
-				elif (medias_list[i] <= inferior):
-					probabilidade_tipo.probabilidade_por_limite_list.append(100 * (distribuicao_normal(superior, medias_list[i], desvios_padroes_list[i]) - distribuicao_normal(inferior, medias_list[i], desvios_padroes_list[i])))	
-			
-			probabilidade_tipo_list.append(probabilidade_tipo)
+        resultado_query = self.executa_query(query)
 
-		return probabilidade_tipo_list
-	
-	def media_desvio_tipos_acidentes(self):
-		query = """SELECT tipo, quantidade_ocorrencias, ano
-				 FROM estatisticas_tipo 
-				 ORDER BY tipo, ano ; """		
-		
-		data_frame = self.executa_query(query, get_data_frame=True)
-		medias_list = data_frame.groupby('tipo')['quantidade_ocorrencias'].mean()
-		desvios_padroes_list = data_frame.groupby('tipo')['quantidade_ocorrencias'].std()
-		media_desvio_tipos_acidentes_list = []
-		for i in range(0, len(medias_list)):
-			media_desvio_tipos_acidentes = MediaDesvioAcidentes()
-			media_desvio_tipos_acidentes.media = medias_list[i]
-			media_desvio_tipos_acidentes.desvio = desvios_padroes_list[i]
+        tipos_acidentes_ano_list = []
+        ultimo_tipo = ''
 
-			media_desvio_tipos_acidentes_list.append(media_desvio_tipos_acidentes)
+        for (tipo, quantidade_ocorrencias, ano) in zip(
+            resultado_query['tipo'].values(),
+            resultado_query['quantidade_ocorrencias'].values(),
+            resultado_query['ano'].values()
+        ):
+            tipo = tipo.decode('iso-8859-1').encode('utf8')
+            if (ultimo_tipo != tipo):
+                tipos_acidentes_ano = AcidentesAno()
+                tipos_acidentes_ano_list.append(tipos_acidentes_ano)
+                tipos_acidentes_ano.tipo = tipo
+                ultimo_tipo = tipo
 
-		return media_desvio_tipos_acidentes_list
+            tipos_acidentes_ano.ano_list.append(ano)
+            tipos_acidentes_ano.quantidade_ocorrencias_list.append(
+                quantidade_ocorrencias
+            )
+
+        return tipos_acidentes_ano_list
+
+    def probabilidade_tipos_acidentes(self):
+        query = """SELECT tipo, quantidade_ocorrencias, ano
+                FROM estatisticas_tipo
+                ORDER BY tipo, ano ; """
+
+        data_frame = self.executa_query(query, get_data_frame=True)
+
+        medias_list = data_frame.groupby(
+            'tipo'
+        )['quantidade_ocorrencias'].mean()
+        desvios_padroes_list = data_frame.groupby(
+            'tipo'
+        )['quantidade_ocorrencias'].std()
+
+        probabilidade_tipo_list = []
+
+        for i in range(0, len(medias_list)):
+            probabilidade_tipo = ProbabilidadeAcidentes()
+            probabilidade_tipo.tipo = medias_list.keys()[
+                i].decode('iso-8859-1').encode('utf8')
+
+            limites = [
+                (0, 1000),
+                (1001, 5000),
+                (5001, 10000),
+                (10001, 50000),
+                (50001, sys.maxint)
+            ]
+            for (inferior, superior) in limites:
+                if (medias_list[i] >= inferior and medias_list[i] <= superior):
+                    probabilidade_tipo.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) + distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+                elif (medias_list[i] >= inferior):
+                    probabilidade_tipo.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) - distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+                elif (medias_list[i] <= inferior):
+                    probabilidade_tipo.probabilidade_por_limite_list.append(
+                        100 * (
+                            distribuicao_normal(
+                                superior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            ) - distribuicao_normal(
+                                inferior,
+                                medias_list[i],
+                                desvios_padroes_list[i]
+                            )
+                        )
+                    )
+
+            probabilidade_tipo_list.append(
+                probabilidade_tipo
+            )
+
+        return probabilidade_tipo_list
+
+    def media_desvio_tipos_acidentes(self):
+        query = """SELECT tipo, quantidade_ocorrencias, ano
+                FROM estatisticas_tipo
+                ORDER BY tipo, ano ; """
+
+        data_frame = self.executa_query(query, get_data_frame=True)
+
+        medias_list = data_frame.groupby(
+            'tipo'
+        )['quantidade_ocorrencias'].mean()
+        desvios_padroes_list = data_frame.groupby(
+            'tipo'
+        )['quantidade_ocorrencias'].std()
+
+        media_desvio_tipos_acidentes_list = []
+
+        for i in range(0, len(medias_list)):
+            media_desvio_tipos_acidentes = MediaDesvioAcidentes()
+            media_desvio_tipos_acidentes.media = medias_list[i]
+            media_desvio_tipos_acidentes.desvio = desvios_padroes_list[i]
+
+            media_desvio_tipos_acidentes_list.append(
+                media_desvio_tipos_acidentes
+            )
+
+        return media_desvio_tipos_acidentes_list
