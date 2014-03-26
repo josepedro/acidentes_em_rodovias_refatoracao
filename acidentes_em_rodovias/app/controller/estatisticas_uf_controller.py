dcb5e270b0d3d1e105dc1b1d5e9366c2d9b7fee3
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 18:56:16 -0300

    Applying code patterns in estatisticas_uf_controller.py

--- acidentes_em_rodovias/app/controller/estatisticas_uf_controller.py
+++ acidentes_em_rodovias/app/controller/estatisticas_uf_controller.py
@@ -1,36 +1,62 @@
-# -*- coding: utf-8 -*- 
-import sys, os, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
 
+import sys
+import os
+import inspect
 import MySQLdb
+import logging
+
 from django.utils.datastructures import MultiValueDictKeyError
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+
 from exception.validation_exceptions import *
 from exception.internal_exceptions import *
-from models.dao.uf_acidentes_dao import *
-from datetime import datetime
-import logging
 
+from models.dao.uf_acidentes_dao import *
+
+from datetime import datetime
+
+# Adding upper directories to the Python Path
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
+
+# Logging config
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
+
 def acidentes_uf(request):
-	try:
-		data = datetime.now()
-		uf_dao = UFAcidentesDAO()
-		uf_acidentes_geral = uf_dao.acidentes_uf_geral()
-		uf_acidentes_ano = uf_dao.acidentes_uf_ano()
+    try:
+        data = datetime.now()
+        uf_dao = UFAcidentesDAO()
+        uf_acidentes_geral = uf_dao.acidentes_uf_geral()
+        uf_acidentes_ano = uf_dao.acidentes_uf_ano()
 
-	except (MySQLdb.Error, ResultadoConsultaNuloError), e:
-		logger.error(str(e))
-		erro = "Ocorreu um erro no sistema, tente novamente mais tarde!"
-		return render_to_response("index.html", {'erro' : erro}, context_instance=RequestContext(request))
+    except (MySQLdb.Error, ResultadoConsultaNuloError), e:
+        logger.error(str(e))
+        erro = "Ocorreu um erro no sistema, tente novamente mais tarde!"
+        return render_to_response(
+            "index.html", {
+                'erro': erro
+            }, context_instance=RequestContext(request)
+        )
 
-	return  render_to_response("uf_acidentes.html",{'ano':range(2007, data.year+1),'uf_acidentes_ano':uf_acidentes_ano, 
-		'uf_acidentes_geral':uf_acidentes_geral[:10],
-		'uf_acidentes_geral_mapa':uf_acidentes_geral
-		}, context_instance=RequestContext(request))
+    return render_to_response(
+        "uf_acidentes.html", {
+            'ano': range(2007, data.year + 1),
+            'uf_acidentes_ano': uf_acidentes_ano,
+            'uf_acidentes_geral': uf_acidentes_geral[:10],
+            'uf_acidentes_geral_mapa': uf_acidentes_geral
+        }, context_instance=RequestContext(request)
+    )
