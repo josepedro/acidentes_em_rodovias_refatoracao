6ec84aa25c0c918649e8ca1c829f51041abc3d36
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 20:12:05 -0300

    Applying code patterns in tests_controller_estatistica_tipocausa.py

--- acidentes_em_rodovias/app/tests/tests_controller_estatistica_tipocausa.py
+++ acidentes_em_rodovias/app/tests/tests_controller_estatistica_tipocausa.py
@@ -1,32 +1,48 @@
-# -*- coding: utf-8 -*- 
-import sys, os, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+import sys
+import os
+import inspect
 
 from django.test import SimpleTestCase
 from django.template import RequestContext, TemplateDoesNotExist
+
 from controller import estatisticas_causas_controller as ctrl
+
 from _mysql_exceptions import *
 
-class TestTipoCausa(SimpleTestCase):
-	"""docstring for TestTipoCausa"""
-	def setUp(self):    #configura ambiente para teste
-		#descobre qual metodo será chamado e formata a saída
-		func = str(self.id).split('=')[-1][:-2]
-		func = func.split('test_')[-1]
-		func = func.replace('_',' ')
-		out = '\rTeste de ' + func + ' '
-		out = out.ljust(65,'-')
-		sys.stderr.write(out)
-		self.shortDescription()
+# Adding upper directories to the Python Path
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
 
-	def tearDown(self):
-		# informa que o teste foi realizado
-		sys.stderr.write('Done\n')
-	
-	def shortDescription(self):
-		return "Teste da classe TestTipoCausa"
-	
-	def test_causas_acidentes(self):
-		self.assertIsNotNone(ctrl.causas_acidentes(None))
+
+class TestTipoCausa(SimpleTestCase):
+
+    """docstring for TestTipoCausa"""
+
+    def setUp(self):
+        func = str(self.id).split('=')[-1][:-2]
+        func = func.split('test_')[-1]
+        func = func.replace('_', ' ')
+        out = '\rTeste de ' + func + ' '
+        out = out.ljust(65, '-')
+        sys.stderr.write(out)
+        self.shortDescription()
+
+    def tearDown(self):
+        sys.stderr.write('Done\n')
+
+    def shortDescription(self):
+        return "Teste da classe TestTipoCausa"
+
+    def test_causas_acidentes(self):
+        self.assertIsNotNone(ctrl.causas_acidentes(None))
