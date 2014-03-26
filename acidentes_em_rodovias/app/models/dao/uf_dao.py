8190130efcde756c301e648a2080c6e69e4da1f7
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 00:12:27 -0300

    Applying code patterns in uf_dao.py

--- acidentes_em_rodovias/app/models/dao/uf_dao.py
+++ acidentes_em_rodovias/app/models/dao/uf_dao.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
 from generico_dao import GenericoDAO
-#from model.uf import *
+
 
 class UfDAO(GenericoDAO):
-	def lista_ufs(self, limite=0):
-		if(limite != 0):
-			limite = 'LIMIT %s' % limite
-		else:
-			limite = ''
-		query = """SELECT tufuf, tufdenominacao
-				FROM uf ORDER BY tufdenominacao %s;""" % limite
 
-		return self.transforma_dicionario_em_objetos(self.executa_query(query), "Uf", "uf")
+    """UF DAO"""
 
+    def lista_ufs(self, limite=0):
+        if(limite != 0):
+            limite = 'LIMIT %s' % limite
+        else:
+            limite = ''
+        query = """SELECT tufuf, tufdenominacao
+                FROM uf ORDER BY tufdenominacao %s;""" % limite
 
+        return self.transforma_dicionario_em_objetos(
+            self.executa_query(query),
+            "Uf",
+            "uf"
+        )
