c853b380b885236ccb86318d07daa2aa31cec19c
msfernandes <matheus.souza.fernandes@gmail.com>
2014-03-26 20:27:52 -0300

    Applying code patterns in tests_dao_causas_acidentes.py

--- acidentes_em_rodovias/app/tests/tests_dao_causas_acidentes.py
+++ acidentes_em_rodovias/app/tests/tests_dao_causas_acidentes.py
@@ -1,62 +1,91 @@
-# -*- coding: utf-8 -*- 
-import sys, os, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+import sys
+import os
+import inspect
 
 from django.test import SimpleTestCase
 from django.core.urlresolvers import reverse, resolve
-from models.dao import uf_dao, causas_acidentes_dao, ocorrencia_basica_dao, pessoas_acidentes_dao, causas_acidentes_dao, municipio_dao, generico_dao
+
+from models.dao import *
+
 from _mysql_exceptions import OperationalError, ProgrammingError
+
 from exception.internal_exceptions import *
 
+# Adding upper directories to the Python Path
+current_path = os.path.dirname(os.path.abspath('..'))
+sys.path.append(current_path)
+current_path = os.path.dirname(os.path.abspath('.'))
+sys.path.append(current_path)
+
+
 class TestCausasAcidentes(SimpleTestCase):
-	"""docstring for TestUF"""
-	def setUp(self):    #configura ambiente para teste
-		self.dao = causas_acidentes_dao.CausasAcidentesDAO()
-		#descobre qual metodo será chamado e formata a saída
-		func = str(self.id).split('=')[-1][:-2]
-		func = func.split('test_')[-1]
-		func = func.replace('_',' ')
-		out = '\rTeste de ' + func + ' '
-		out = out.ljust(65,'-')
-		sys.stderr.write(out)
-		self.shortDescription()
 
-	def tearDown(self):
-		# informa que o teste foi realizado
-		sys.stderr.write('Done\n')
+    """docstring for TestUF"""
 
-	def shortDescription(self):
-		return "Teste da classe TestCausasAcidentesDAO"
+    def setUp(self):
+        self.dao = causas_acidentes_dao.CausasAcidentesDAO()
+        func = str(self.id).split('=')[-1][:-2]
+        func = func.split('test_')[-1]
+        func = func.replace('_', ' ')
+        out = '\rTeste de ' + func + ' '
+        out = out.ljust(65, '-')
+        sys.stderr.write(out)
+        self.shortDescription()
 
-	def test_causas_acidentes(self):
-		causas_acidentes_list = self.dao.causas_acidentes()
-		
-		#self.assertEquals(len(causas_acidentes_list), 16)
+    def tearDown(self):
+        sys.stderr.write('Done\n')
 
-		descricao_causas_acidentes = [i.causa for i in causas_acidentes_list]
-		self.assertIn("Outras", descricao_causas_acidentes)
+    def shortDescription(self):
+        return "Teste da classe TestCausasAcidentesDAO"
 
-	def test_causas_acidentes_ano(self):
-		causas_acidentes_ano_list = self.dao.causas_acidentes_ano()
-		
-		anos = causas_acidentes_ano_list[0].ano_list
-		self.assertEquals([2007, 2008, 2009, 2010, 2011, 2012, 2013], anos)
+    def test_causas_acidentes(self):
+        causas_acidentes_list = self.dao.causas_acidentes()
 
-		descricao_causas_acidentes_ano = [i.causa for i in causas_acidentes_ano_list]
-		self.assertIn("Outras", descricao_causas_acidentes_ano)
+        descricao_causas_acidentes = []
+        for acidente in causas_acidentes_list:
+            descricao_causas_acidentes.append(acidente.causa)
 
-	def test_probabilidade_causas_acidentes(self):
-		probabilidade_causas_acidentes_list = self.dao.probabilidade_causas_acidentes()
+        self.assertIn("Outras", descricao_causas_acidentes)
 
-		for probabilidade_causas_acidentes in probabilidade_causas_acidentes_list:
-			for probabilidade in probabilidade_causas_acidentes.probabilidade_por_limite_list:
-				self.assertTrue(probabilidade >= 0 and probabilidade <= 100)
+    def test_causas_acidentes_ano(self):
+        causas_acidentes_ano_list = self.dao.causas_acidentes_ano()
 
-	def test_media_desvio_causas_acidentes(self):
-		media_desvio_causas_acidentes_list = self.dao.media_desvio_causas_acidentes()		
+        anos = causas_acidentes_ano_list[0].ano_list
+        self.assertEquals([
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013
+        ], anos)
 
-		for media_desvio_causas_acidentes in media_desvio_causas_acidentes_list:
-			self.assertTrue(media_desvio_causas_acidentes.media > 0)
-			self.assertTrue(media_desvio_causas_acidentes.desvio > 0)
+        descricao_causas_acidentes_ano = []
+        for acidente in causas_acidentes_ano_list:
+            descricao_causas_acidentes_ano.append(acidente.causa)
+
+        self.assertIn("Outras", descricao_causas_acidentes_ano)
+
+    def test_probabilidade_causas_acidentes(self):
+        probabilidade_causas_list = self.dao.probabilidade_causas_acidentes()
+
+        for causa in probabilidade_causas_list:
+            for probabilidade in causa.probabilidade_por_limite_list:
+                self.assertTrue(probabilidade >= 0 and probabilidade <= 100)
+
+    def test_media_desvio_causas_acidentes(self):
+        media_desvio_list = self.dao.media_desvio_causas_acidentes()
+
+        for media_desvio_causas_acidentes in media_desvio_list:
+            self.assertTrue(media_desvio_causas_acidentes.media > 0)
+            self.assertTrue(media_desvio_causas_acidentes.desvio > 0)
