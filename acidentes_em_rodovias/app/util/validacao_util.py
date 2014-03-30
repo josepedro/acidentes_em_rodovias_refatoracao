ff8a369e971711b4ca92113621812395ef8e868f
Ziul <ziuloliveira@gmail.com>
2014-03-30 17:02:02 -0300

    Renaming some imports

--- acidentes_em_rodovias/app/util/validacao_util.py
+++ acidentes_em_rodovias/app/util/validacao_util.py
@@ -12,12 +12,12 @@ Calc some statistics methods.
 import sys
 import os
 import inspect
-current_dir = os.path.dirname(
+_current_dir = os.path.dirname(
     os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+_parent_dir = os.path.dirname(_current_dir)
+sys.path.append(_parent_dir)
 
-import re
+from re import search
 from exception.validation_exceptions import *
 from exception.internal_exceptions import *
 
@@ -25,7 +25,7 @@ from exception.internal_exceptions import *
 def valida_data(data):
     """ Validate date """
 
-    if (re.search('^[0-3]\d/[01]\d/\d{4}$', data) is None
+    if (search('^[0-3]\d/[01]\d/\d{4}$', data) is None
             or int(data[0:2]) >= 32
             or int(data[3:5]) >= 13):
         raise DataInvalidaError("Data invalida inserida: " + data)
@@ -34,7 +34,7 @@ def valida_data(data):
 def valida_caracteres(palavra):
     """ Validate a sequence of characters """
 
-    if re.search('^[\w\s]+$', str(palavra) is None):
+    if search('^[\w\s]+$', str(palavra) is None):
         raise ParametroInseguroClienteError(
             "Parametro invalido inserido: " +
             palavra)
