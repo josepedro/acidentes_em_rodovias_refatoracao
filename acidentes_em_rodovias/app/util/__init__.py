47a23dcba798060cdf03da2cc8f71ed673ccb7e9
Ziul <ziuloliveira@gmail.com>
2014-03-30 17:09:48 -0300

    Including Docstring to package

---
+++ acidentes_em_rodovias/app/util/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Acidentes em Rodovias - Util functions
+
+ Universidade de Brasilia - FGA
+ Técnicas de Programação, 1/2014
+
+ GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+This package has some useful methods to calculate some statistics or
+validate some returns.
+"""
+
+from os import environ as _environ
+
+__copyright__ = "Copyright 2007, The Cogent Project"
+__license__ = "GPL"
+__date__ = '2013-2014'
+__version__ = "1.0.1"
+__author__ = """Matheus Fonseca, Rodrigo Lopes Rincon, Paulo Borba,
+Gustavo Jaruga Cruz, Matheus Fernandes, José Pedro Santana,
+Carlos Oliveira, Bruno Amorim and Luiz Fernando Gomes de Oliveira"""
