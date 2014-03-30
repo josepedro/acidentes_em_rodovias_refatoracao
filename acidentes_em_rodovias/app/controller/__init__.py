9aea58c33bab2493edf1156cd6f044a3104d41a2
Ziul <ziuloliveira@gmail.com>
2014-03-30 01:38:37 -0300

    including docstring on __init__ from controler

---
+++ acidentes_em_rodovias/app/controller/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Controller
+
+ Universidade de Brasilia - FGA
+ Técnicas de Programação, 1/2014
+
+ GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+ Controller from an application of data analysis from police reports of
+ accidents in Brazilian federal highways.
+"""
+__copyright__ = "Copyright 2007, The Cogent Project"
+__license__ = "GPL"
+__date__ = '2013-2014'
+__version__ = "1.0.1"
+__author__ = """Matheus Fonseca, Rodrigo Lopes Rincon, Paulo Borba,
+Gustavo Jaruga Cruz, Matheus Fernandes, José Pedro Santana,
+Carlos Oliveira, Bruno Amorim and Luiz Fernando Gomes de Oliveira"""
