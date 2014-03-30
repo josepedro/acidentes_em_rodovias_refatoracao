68eae2c75bb263d77d1c837c1902d0907f111848
Ziul <ziuloliveira@gmail.com>
2014-03-30 00:52:42 -0300

    Initial commit from Docstring support and corrections on design

---
+++ acidentes_em_rodovias/app/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Acidentes em Rodovias
+
+ Universidade de Brasilia - FGA
+ Técnicas de Programação, 1/2014
+
+ GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+ An application of data analysis from police reports of accidents in
+ Brazilian federal highways.
+"""
+
+from os import environ as _environ
+
+__copyright__ = "Copyright 2007, The Cogent Project"
+__license__ = "GPL"
+__date__ = '2013-2014'
+__version__ = "1.0.1"
+__author__ = """Matheus Fonseca, Rodrigo Lopes Rincon, Paulo Borba,
+Gustavo Jaruga Cruz, Matheus Fernandes, José Pedro Santana,
+Carlos Oliveira, Bruno Amorim and Luiz Fernando Gomes de Oliveira"""
+
+if not "DJANGO_SETTINGS_MODULE" in _environ:
+    _environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "acidentes_em_rodovias.settings")
