1e4a06275613fc8eb5911b0c9bdd8af118dba0d7
Ziul <ziuloliveira@gmail.com>
2014-03-30 17:28:46 -0300

    Including Docstring to package

--- acidentes_em_rodovias/app/exception/validation_exceptions.py
+++ acidentes_em_rodovias/app/exception/validation_exceptions.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-#
-# Universidade de Brasilia - FGA
-# Técnicas de Programação, 1/2014
-#
-# Acidentes em Rodovias, 2013-2014
-# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
-#
+#!/usr/bin/env python
+# coding: utf-8
 
-"""@package validation exceptions
-Exceções de validação.
+"""Acidentes em Rodovias - validation exceptions
 
-Este modulo contem classes de controle de exceção para validação
+ Universidade de Brasilia - FGA
+ Técnicas de Programação, 1/2014
+
+This module contains control classes of exception for validation
 """
+
 from exceptions import Exception
 
 
