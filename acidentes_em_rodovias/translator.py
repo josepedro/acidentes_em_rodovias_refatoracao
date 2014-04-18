45bfb955443291d1f19186ae7bbddaf772a2db0f
Ziul <ziuloliveira@gmail.com>
2014-04-18 14:06:18 -0300

    open file once now

--- acidentes_em_rodovias/translator.py
+++ acidentes_em_rodovias/translator.py
@@ -13,9 +13,9 @@ import logging
 import os
 import re
 
-_MAX_PEERS = 50
+_MAX_PEERS = 30
 
-
+os.remove('traducoes.log')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler('traducoes.log')
@@ -32,12 +32,11 @@ def fileWalker(ext, dirname, names):
             ext[1].append(os.path.join(dirname, f))
 
 
-def encontre_text(arquivo):
+def encontre_text(file):
     """
     find on the string the works wich have '_' on it
     """
-    with open(arquivo) as file:
-        text = file.read()
+    text = file.read()
     return re.findall(r"\w+(?<=_)\w+", text)
     #return re.findall(r"\"\w+\"", text)
 
@@ -56,12 +55,14 @@ def traduza_palavra(txt):
     return txt.replace(' ', '_')  # .lower()
 
 
-def subistitua(File, txt, novo_txt):
+def subistitua(file, txt, novo_txt):
     """
     should rewrite the file with the new text in the future
     """
-    logger.info('%s -> %s [%s]' % (txt, novo_txt, File))
-    pass
+    text = file.read()
+    text.replace(txt, novo_txt)
+    file.seek(0)  # rewind
+    file.write(text)
 
 
 def magica(File):
@@ -70,10 +71,13 @@ def magica(File):
     one element from list os files
     """
     #print '\n---- File %s' % File #inviavel em multithread
-    list_txt = encontre_text(File)
-    for txt in list_txt:
-        novo_txt = traduza_palavra(txt)
-        subistitua(File, txt, novo_txt)
+    with open(File, "r+") as file:
+        list_txt = encontre_text(file)
+        for txt in list_txt:
+            novo_txt = traduza_palavra(txt)
+            logger.info('%s -> %s [%s]' % (txt, novo_txt, File))
+            #subistitua(file, txt, novo_txt)
+        file.close()
     print File.ljust(70) + '[OK]'.rjust(5)
 
 if __name__ == '__main__':
