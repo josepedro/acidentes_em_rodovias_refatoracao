7f4783ab40f963746b7d19826a24e0b40041fe79
Ziul <ziuloliveira@gmail.com>
2014-04-18 02:53:03 -0300

    stabilized translation and added stringdoc

--- acidentes_em_rodovias/translator.py
+++ acidentes_em_rodovias/translator.py
@@ -1,39 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+ Script to helps on translate some code's methods from
+ portuguese to english.
+ """
 
+
+from multiprocessing import Pool
 from goslate import Goslate
 import fnmatch
 import os
 import re
 
+_MAX_PEERS = 50
+
 
 def fileWalker(ext, dirname, names):
-    '''
-    checks files in names'''
+    """
+    Find the files with the correct extension
+    """
     pat = "*" + ext[0]
     for f in names:
         if fnmatch.fnmatch(f, pat):
             ext[1].append(os.path.join(dirname, f))
 
 
-def find_text(arquivo):
-    #return raw_input('Entre com o texto a ser traduzido: ')
-    #tesouros = ()
+def encontre_text(arquivo):
+    """
+    find on the string the works wich have '_' on it
+    """
     text = open(arquivo).read()
     return re.findall(r"\w+(?<=_)\w+", text)
 
 
-def traduza(txt):
-    txt = txt.replace('_', ' ')
+def traduza_palavra(txt):
+    """
+        Translate the word/phrase to english
+    """
+    if txt[0] != '_':
+        txt = txt.replace('_', ' ')
     gs = Goslate()
-    txt = gs.translate(txt, 'en', gs.detect(txt))
-    return txt.replace(' ', '_')
+    #txt = gs.translate(txt, 'en', gs.detect(txt))
+    txt = gs.translate(txt, 'en', 'pt-br')  # garantindo idioma tupiniquim
+    return txt.replace(' ', '_').lower()
 
 
 def subistitua(txt, novo_txt):
+    """
+    should rewrite the file with the new text in the future
+    """
     print '%s -> %s' % (txt, novo_txt)
 
+
+def magica(File):
+    """
+    Thread Pool. Every single thread should play around here with
+    one element from list os files
+    """
+    #print '\n---- File %s' % File #inviavel em multithread
+    list_txt = encontre_text(File)
+    for txt in list_txt:
+        novo_txt = traduza_palavra(txt)
+        subistitua(txt, novo_txt)
 
 if __name__ == '__main__':
     root = './app'
@@ -41,9 +70,7 @@ if __name__ == '__main__':
     files = []
     os.path.walk(root, fileWalker, [ex, files])
     #print files
-    for File in files:
-        print '---- File %s' % File
-        list_txt = find_text(File)
-        for txt in list_txt:
-            novo_txt = traduza(txt)
-            subistitua(txt, novo_txt)
+    _pool = Pool(processes=_MAX_PEERS)
+    result = _pool.map_async(magica, files[:8])
+    result.wait()
+    #for File, i in zip(files, range(3)):
