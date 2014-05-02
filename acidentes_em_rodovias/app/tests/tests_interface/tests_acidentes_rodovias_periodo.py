6a126f7de2a6c0e0f99eff8e9b342a99f1210c8c
msfernandes <matheus.souza.fernandes@gmail.com>
2014-05-02 01:31:19 -0300

    Setting encoding

--- acidentes_em_rodovias/app/tests/tests_interface/tests_acidentes_rodovias_periodo.py
+++ acidentes_em_rodovias/app/tests/tests_interface/tests_acidentes_rodovias_periodo.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf8 -*-
+# -*- encoding: utf-8 -*-
 
 import unittest
 from selenium import webdriver
@@ -7,91 +7,94 @@ import time
 from datetime import date
 import sys
 
+
 class AcidentesRodoviasPeriodoTestCase(unittest.TestCase):
-	porta = '8000'
-	mes = {'Janeiro' : 1, 'Fevereiro' : 2, 'Março' : 3, 'Abril' : 4, 'Maio' : 5, 'Junho' : 6, 'Julho' : 7, 
-			'Agosto' : 8, 'Setembro' : 9, 'Outubro' : 10, 'Novembro' : 11, 'Dezembro' : 12}
+    porta = '8000'
+    mes = {
+        'Janeiro': 1, 'Fevereiro': 2, 'Março': 3, 'Abril': 4, 'Maio': 5, 'Junho': 6, 'Julho': 7,
+        'Agosto': 8, 'Setembro': 9, 'Outubro': 10, 'Novembro': 11, 'Dezembro': 12}
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.addCleanup(self.browser.quit)
-		self.browser.get('http://127.0.0.1:'+self.porta+'/acidentes_rodovias/periodo')
-		if (self.browser.title == u'Falha no carregamento da página'):
-			sys.stderr.write("\nInicie a aplicação na porta "+self.porta+" ou altere o atributo 'porta' no TestCase")
-			exit(0)
-		else:
-			func = str(self.id).split('=')[-1][:-2]
-			func = func.split('test_')[-1]
-			func = func.replace('_',' ')
-			out = '\rTeste de Interface [periodo]: ' + func + ' '
-			out = out.ljust(65,'-')
-			sys.stderr.write(out)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.addCleanup(self.browser.quit)
+        self.browser.get(
+            'http://127.0.0.1:' + self.porta + '/acidentes_rodovias/periodo')
+        if (self.browser.title == u'Falha no carregamento da página'):
+            sys.stderr.write("\nInicie a aplicação na porta " +
+                             self.porta + " ou altere o atributo 'porta' no TestCase")
+            exit(0)
+        else:
+            func = str(self.id).split('=')[-1][:-2]
+            func = func.split('test_')[-1]
+            func = func.replace('_', ' ')
+            out = '\rTeste de Interface [periodo]: ' + func + ' '
+            out = out.ljust(65, '-')
+            sys.stderr.write(out)
 
-	def tearDown(self):
-		# informa que o teste foi realizado
-		sys.stderr.write('Done\n')
+    def tearDown(self):
+        # informa que o teste foi realizado
+        sys.stderr.write('Done\n')
 
+    def test_pagetitle(self):
+        self.assertIn('Acidentes em Rodovias', self.browser.title)
 
-	def test_pagetitle(self):
-		self.assertIn('Acidentes em Rodovias', self.browser.title)
-		
-   
-	def test_date_periodo_invalido(self):
-		de = '12345678'
-		ate = '23456789'
-		campo_de = self.browser.find_element_by_id('de')
-		campo_ate = self.browser.find_element_by_id('ate')
-		
-		campo_de.send_keys(de)
-		campo_ate.send_keys(ate)
+    def test_date_periodo_invalido(self):
+        de = '12345678'
+        ate = '23456789'
+        campo_de = self.browser.find_element_by_id('de')
+        campo_ate = self.browser.find_element_by_id('ate')
 
-		self.browser.find_element_by_id('btn_submit').click()	
+        campo_de.send_keys(de)
+        campo_ate.send_keys(ate)
 
-		modal_error = self.browser.find_element_by_id('modalErro')
+        self.browser.find_element_by_id('btn_submit').click()
 
-		self.assertIsNotNone(modal_error)
+        modal_error = self.browser.find_element_by_id('modalErro')
 
-	def test_date_periodo_valido(self):
-		de = '12/04/2008'
-		ate = '30/06/2008'
+        self.assertIsNotNone(modal_error)
 
-		campo_de = self.browser.find_element_by_id('de')
-		campo_ate = self.browser.find_element_by_id('ate')
+    def test_date_periodo_valido(self):
+        de = '12/04/2008'
+        ate = '30/06/2008'
 
-		campo_de.send_keys(de)
-		campo_ate.send_keys(ate)
+        campo_de = self.browser.find_element_by_id('de')
+        campo_ate = self.browser.find_element_by_id('ate')
 
-		self.browser.find_element_by_id('btn_submit').click()
+        campo_de.send_keys(de)
+        campo_ate.send_keys(ate)
 
-		result_list = self.browser.find_elements_by_class_name('resultados')
+        self.browser.find_element_by_id('btn_submit').click()
 
-		self.assertIsNotNone(result_list)
+        result_list = self.browser.find_elements_by_class_name('resultados')
 
-	def test_verifica_periodo(self):
-		de = '12/04/2008'
-		ate = '30/06/2008'
+        self.assertIsNotNone(result_list)
 
-		campo_de = self.browser.find_element_by_id('de')
-		campo_ate = self.browser.find_element_by_id('ate')
+    def test_verifica_periodo(self):
+        de = '12/04/2008'
+        ate = '30/06/2008'
 
-		campo_de.send_keys(de)
-		campo_ate.send_keys(ate)
+        campo_de = self.browser.find_element_by_id('de')
+        campo_ate = self.browser.find_element_by_id('ate')
 
-		de = de.split('/')
-		ate = ate.split('/')
+        campo_de.send_keys(de)
+        campo_ate.send_keys(ate)
 
-		de = date(int(de[2]), int(de[1]), int(de[0]))
-		ate = date(int(ate[2]), int(ate[1]), int(ate[0]))
+        de = de.split('/')
+        ate = ate.split('/')
 
-		self.browser.find_element_by_id('btn_submit').click()
+        de = date(int(de[2]), int(de[1]), int(de[0]))
+        ate = date(int(ate[2]), int(ate[1]), int(ate[0]))
 
-		result_list = self.browser.find_elements_by_class_name('data_registro')
+        self.browser.find_element_by_id('btn_submit').click()
 
-		for element in result_list:
-			data_registro = element.text
-			data_registro = data_registro.split()
-			data_registro = date(int(data_registro[4]), self.mes.get(data_registro[2]), int(data_registro[0]))
-			self.assertTrue(de <= data_registro <= ate)
+        result_list = self.browser.find_elements_by_class_name('data_registro')
+
+        for element in result_list:
+            data_registro = element.text
+            data_registro = data_registro.split()
+            data_registro = date(int(data_registro[4]), self.mes.get(
+                data_registro[2]), int(data_registro[0]))
+            self.assertTrue(de <= data_registro <= ate)
 
 if __name__ == '__main__':
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
