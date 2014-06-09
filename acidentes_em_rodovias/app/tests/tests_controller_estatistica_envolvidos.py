a9945d71c4be543b979976da209b8b62c8e30448
Ziul <ziuloliveira@gmail.com>
2014-06-08 22:47:51 -0300

    completed test estatisticas_envolvidos_controller

--- acidentes_em_rodovias/app/tests/tests_controller_estatistica_envolvidos.py
+++ acidentes_em_rodovias/app/tests/tests_controller_estatistica_envolvidos.py
@@ -17,14 +17,27 @@ from django.template import RequestContext, TemplateDoesNotExist
 from app.controller import estatisticas_envolvidos_controller as ctrl
 
 from _mysql_exceptions import *
+from app import myconfiguration
 
 
 class Test_Envolvidos(Controller_Tests):
 
     """docstring for Test_Envolvidos"""
 
-    def shortDescription(self):
-        return "Teste da classe Test_Envolvidos"
+    def tearDown(self):
+        myconfiguration.DB_PASS = self.db_password
+
 
     def test_ocorrencias_e_envolvidos(self):
         self.assertIsNotNone(ctrl.ocorrencias_e_envolvidos(None))
+
+    def test_database_connection(self):
+        myconfiguration.DB_PASS = ''
+
+        with self.assertRaises(OperationalError):
+            response = self.client.get('/acidentes_rodovias/estatisticas/acidentes-sexo')
+            self.assertTemplateUsed(response, 'index.html')
+            self.assertEquals(response.context[-1]['erro'],
+                          "Ocorreu um erro no sistema, tente novamente mais tarde!"
+                          )
+
