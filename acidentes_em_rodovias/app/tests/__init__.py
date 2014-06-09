77feb0cf968c8451d66d19f5b7fd479aab9ae759
msfernandes <matheus.souza.fernandes@gmail.com>
2014-06-08 22:28:49 -0300

    Creating estatistica_tipo_controller test case

--- acidentes_em_rodovias/app/tests/__init__.py
+++ acidentes_em_rodovias/app/tests/__init__.py
@@ -11,6 +11,7 @@ from tests_controller_estatistica_causa import TestCausa
 from tests_controller_estatistica_sexo import Test_Sexo
 from tests_controller_estatistica_envolvidos import Test_Envolvidos
 from tests_controller_estatistica_br import TestEstatisticaBR
+from tests_controller_estatistica_tipo import TestControllerEstatisticaTipo
 # ------------DAO---------
 from tests_dao import TestDAO
 from tests_dao_municipio import TestMunicipio
---
+++ acidentes_em_rodovias/app/tests/tests_controller_estatistica_tipo.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+import sys
+import os
+import inspect
+
+from app.tests.tests_basic import Controller_Tests
+from django.template import RequestContext, TemplateDoesNotExist
+
+from app import myconfiguration
+
+
+class TestControllerEstatisticaTipo(Controller_Tests):
+
+    """docstring for TestControllerEstatisticaTipo"""
+
+    def shortDescription(self):
+        return "Teste da classe TestControllerEstatisticaTipo"
+
+    def tearDown(self):
+        myconfiguration.DB_PASS = self.db_password
+
+    def test_response_causas_acidentes(self):
+        response = self.client.get(
+            '/acidentes_rodovias/estatisticas/tipos-acidentes'
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tipos_acidentes.html')
+
+    def test_view_return_objects(self):
+        response = self.client.get(
+            '/acidentes_rodovias/estatisticas/tipos-acidentes'
+        )
+        self.assertIsNotNone(response.context[-1]['tipos_acidentes_list'])
+        self.assertIsNotNone(response.context[-1]['tipos_acidentes_ano_list'])
+        self.assertIsNotNone(
+            response.context[-1]['probabilidade_tipos_acidentes_list']
+        )
+        self.assertIsNotNone(response.context[-1]['anos'])
+        self.assertIsNotNone(
+            response.context[-1]['tipos']
+        )
+        self.assertIsNotNone(
+            response.context[-1]['media_desvio_tipos_acidentes_list'])
+
+    def test_database_connection(self):
+        myconfiguration.DB_PASS = ''
+
+        response = self.client.get(
+            '/acidentes_rodovias/estatisticas/tipos-acidentes'
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertEquals(response.context[-1]['erro'],
+                          "Ocorreu um erro no sistema, tente novamente mais tarde!"
+                          )
