ddfdd7da9f790a907af6c2c33e32c5ad156a74df
msfernandes <matheus.souza.fernandes@gmail.com>
2014-06-08 21:06:12 -0300

    Updating tests __init__.py and creating estatistica_br_controller test case

--- acidentes_em_rodovias/app/tests/__init__.py
+++ acidentes_em_rodovias/app/tests/__init__.py
@@ -2,23 +2,24 @@
 
 #------------Controller---------
 # Index
-from tests_controller_index import *
+from tests_controller_index import TestControllerIndex
 # Consulta basica
-from tests_controller_consultabasica_periodo import *
-from tests_controller_consultabasica_regiao import *
+from tests_controller_consultabasica_periodo import Test_Periodo
+from tests_controller_consultabasica_regiao import Test_Regiao
 # Estatisticas
-from tests_controller_estatistica_tipocausa import *
-from tests_controller_estatistica_sexo import *
-from tests_controller_estatistica_envolvidos import *
+from tests_controller_estatistica_tipocausa import TestTipoCausa
+from tests_controller_estatistica_sexo import Test_Sexo
+from tests_controller_estatistica_envolvidos import Test_Envolvidos
+from tests_controller_estatistica_br import TestEstatisticaBR
 # ------------DAO---------
-from tests_dao import *
-from tests_dao_municipio import *
-from tests_dao_UF import *
-from tests_dao_ocorrencia import *
-from tests_dao_causas_acidentes import *
-from tests_dao_tiposacidentes import *
-from tests_dao_envolvidosacidentes import *
-from tests_dao_br_acidentes import *
-from tests_dao_uf_acidentes import *
+from tests_dao import TestDAO
+from tests_dao_municipio import TestMunicipio
+from tests_dao_UF import TestUF
+from tests_dao_ocorrencia import TestOcorrencia
+from tests_dao_causas_acidentes import TestCausasAcidentes
+from tests_dao_tiposacidentes import TestTiposAcidentes
+from tests_dao_envolvidosacidentes import TestEnvolvidosAcidentes
+from tests_dao_br_acidentes import Test_BR_Acidentes
+from tests_dao_uf_acidentes import Test_UF_Acidentes
 # Util
-from tests_util_validacao import *
+from tests_util_validacao import Test_Valida
---
+++ acidentes_em_rodovias/app/tests/tests_controller_estatistica_br.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Universidade de Brasilia - FGA
+# Técnicas de Programação, 1/2014
+#
+# Acidentes em Rodovias, 2013-2014
+# GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
+#
+
+from sys import stderr
+
+from django.test import Client
+
+from app.tests.tests_basic import Controller_Tests
+from app import myconfiguration
+
+
+class TestEstatisticaBR(Controller_Tests):
+
+    def tearDown(self):
+        myconfiguration.DB_PASS = self.db_password
+
+    def test_response_estatistica_br(self):
+        response = self.client.get('/acidentes_rodovias/estatisticas/br')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'br_acidentes.html')
+
+    def test_database_connection(self):
+        myconfiguration.DB_PASS = ''
+
+        response = self.client.get('/acidentes_rodovias/estatisticas/br')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertEquals(response.context[-1]['erro'],
+                          "Ocorreu um erro no sistema, tente novamente mais tarde!"
+                          )
