2e78b728c5bac46b1d4cba3a254eaecb221e7358
msfernandes <matheus.souza.fernandes@gmail.com>
2014-06-16 21:39:37 -0300

    Removing ununsed imports in tests_controller_estatistica_envolvidos.py

--- acidentes_em_rodovias/app/tests/tests_controller_estatistica_envolvidos.py
+++ acidentes_em_rodovias/app/tests/tests_controller_estatistica_envolvidos.py
@@ -6,17 +6,8 @@
 # Acidentes em Rodovias, 2013-2014
 # GitHub: https://github.com/josepedro/acidentes_em_rodovias_refatoracao
 #
-
-import sys
-import os
-import inspect
-
 from app.tests.tests_basic import Controller_Tests
-from django.template import RequestContext, TemplateDoesNotExist
-
 from app.controller import estatisticas_envolvidos_controller as ctrl
-
-from _mysql_exceptions import *
 from app import myconfiguration
 
 
@@ -27,16 +18,16 @@ class Test_Envolvidos(Controller_Tests):
     def tearDown(self):
         myconfiguration.DB_PASS = self.db_password
 
-
     def test_ocorrencias_e_envolvidos(self):
         self.assertIsNotNone(ctrl.ocorrencias_e_envolvidos(None))
 
     def test_database_connection(self):
         myconfiguration.DB_PASS = ''
 
-        response = self.client.get('/acidentes_rodovias/estatisticas/acidentes-sexo')
+        response = self.client.get(
+            '/acidentes_rodovias/estatisticas/acidentes-sexo')
         self.assertTemplateUsed(response, 'index.html')
-        self.assertEquals(response.context[-1]['erro'],
-                      "Ocorreu um erro no sistema, tente novamente mais tarde!"
-                      )
-
+        self.assertEquals(
+            response.context[-1]['erro'],
+            "Ocorreu um erro no sistema, tente novamente mais tarde!"
+        )
