fff31db023a46c3052f9038b749f5b2924e71a41
Ziul <ziuloliveira@gmail.com>
2014-06-19 21:44:42 -0300

    updating dependences

--- acidentes_em_rodovias/app/controller/estatisticas_br_controller.py
+++ acidentes_em_rodovias/app/controller/estatisticas_br_controller.py
@@ -15,7 +15,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from app.exception.internal_exceptions import ResultadoConsultaNuloError
-from app.models.dao.br_acidentes_dao import BRAcidentesDAO
+from app.dao.br_acidentes_dao import BRAcidentesDAO
 
 from datetime import datetime
 
