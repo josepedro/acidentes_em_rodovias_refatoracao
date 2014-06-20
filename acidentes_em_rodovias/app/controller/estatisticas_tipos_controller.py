40ae800866ed0defb7a3855cd276449ca83819db
Ziul <ziuloliveira@gmail.com>
2014-06-19 21:45:36 -0300

    updating dependences

--- acidentes_em_rodovias/app/controller/estatisticas_tipos_controller.py
+++ acidentes_em_rodovias/app/controller/estatisticas_tipos_controller.py
@@ -15,7 +15,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from app.exception.internal_exceptions import ResultadoConsultaNuloError
-from app.models.dao.tipos_acidentes_dao import TiposAcidentesDAO
+from app.dao.tipos_acidentes_dao import TiposAcidentesDAO
 
 # Logging config
 logging.basicConfig()
